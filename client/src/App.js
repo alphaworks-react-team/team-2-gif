7710f916adcaa361d9497fef847935a747c6a75d
dannythedeveloper1 <daniel.girma9@gmail.com>
2021-08-24 17:13:53 -0400

    assign components

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 
 import { useState, useEffect } from "react";
 import axios from "axios";
-import Search from "./Components/Search";
+import Search from "./Components/HomeSearch/Search";
 
 const App = () => {
   const [trending, setTrending] = useState([]);
---
+++ client/src/Components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+//height:100%;
+//width:80%
+//center
+const Main = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Main
