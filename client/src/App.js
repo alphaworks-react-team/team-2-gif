06edd09076542082643ec887a2d00ee672a88f61
jerniceduncan <jerniceduncan@icloud.com>
2021-08-25 10:42:35 -0700

    updated searchPage.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,53 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import "./App.css";
-import axios from 'axios';
-import Search from './Components/HomeSearch/Search';
-import Main from './Components/Main'
-import TrendingPage from './Components/TrendingPage/TrendingPage'
+import axios from "axios";
+import Search from "./Components/HomeSearch/Search";
+import Main from "./Components/Main";
+import TrendingPage from "./Components/TrendingPage/TrendingPage";
+import SearchPage from "./Components/SearchPage/SearchPage";
 
 const App = () => {
-	const [trending, setTrending] = useState([]);
-	const [searchTerm, setSearchTerm] = useState('');
-	const [searchedGifs, setSearchedGifs] = useState([]);
+  const [trending, setTrending] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchedGifs, setSearchedGifs] = useState([]);
 
-	useEffect(() => {
-		axios
-			.get('/api')
-			.then(res => {
-				console.log(res);
-				setTrending(res.data);
-			})
-			.catch(err => console.log(err));
-	}, []);
+  useEffect(() => {
+    axios
+      .get("/api")
+      .then((res) => {
+        console.log(res);
+        setTrending(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
-	const onSearchSubmit = searchTerm => {
-		setSearchTerm(searchTerm);
-		axios
-			.get(`/search/${searchTerm}`)
-			.then(res => {
-				console.log(res);
-				setSearchedGifs(res.data);
-			})
-			.catch(err => console.log(err));
-	};
+  const onSearchSubmit = (searchTerm) => {
+    setSearchTerm(searchTerm);
+    axios
+      .get(`/search/${searchTerm}`)
+      .then((res) => {
+        console.log(res);
+        setSearchedGifs(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
 
-	return (
-		<div className='App'>
-			<Main>
-				<Search onSearchSubmit={onSearchSubmit} />
-			</Main>
-		</div>
-	);
+  return (
+    <div className="App">
+      <Main>
+        <Search onSearchSubmit={onSearchSubmit} />
+        <SearchPage>
+          hello
+          {searchedGifs &&
+            searchedGifs.map((gif, index) => (
+              <div key={index}>
+                <img src={gif.images.fixed_height.url} alt="" />
+              </div>
+            ))}
+        </SearchPage>
+      </Main>
+    </div>
+  );
 };
 
 export default App;
--- client/src/Components/SearchPage/SearchPage.js
+++ client/src/Components/SearchPage/SearchPage.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React from "react";
+import Search from "./../HomeSearch/Search";
+
 //height:80vh
 //width:100%
 const SearchPage = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <div>
+      {/* <form onSubmit={onSubmit}>
+        <input
+          style={styles.input}
+          value={searchTerm}
+          onChange={onChange}
+          type="text"
+          placeholder="search"
+          name="search"
+        />
+        <Button type="submit" bgColor="#007bff" size="small" color="white">
+          Search
+        </Button>
+      </form> */}
+    </div>
+  );
+};
 
-export default SearchPage
+export default SearchPage;
