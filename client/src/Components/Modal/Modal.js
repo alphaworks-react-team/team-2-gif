f559981ef07b01f2ca36fd9c5f584fdac5f5c507
jerniceduncan <jerniceduncan@icloud.com>
2021-08-27 11:03:50 -0700

    updated modal.js

--- client/src/Components/Modal/Modal.js
+++ client/src/Components/Modal/Modal.js
@@ -1,19 +1,24 @@
-const Modal = (props) => {
-  const styleModal = {
-    width: "500px",
-    background: "white",
-    border: "1px solid #ccc",
-    transition: "1.1s ease-out",
-    boxShadow: "-2rem 2rem 2rem rgba(black, 0.2)",
-    filter: "blur(0)",
+import styled from "styled-components";
 
-    opacity: " 1",
-    visibility: props.shown === true ? "visible" : "hidden",
-    position: "fixed",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-  };
+const Modal = styled.modal`
+  width: 500px;
+  border: 3px solid #ccc;
+  transition: 1.1s ease-out;
+  box-shadow: -2rem 2rem 2rem rgba(black, 0.2);
+  filter: blur(0);
+
+  opacity: 1;
+  visibility: ${props.shown === true ? visible : hidden};
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: black;
+  background-position: center;
+  background-size: cover;
+`;
+
+const Modal = (props) => {
   return (
     <div style={styleModal} onClick={props.onClick}>
       {props.children}
