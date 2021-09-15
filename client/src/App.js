ca8e2de30eaf7aead3edca6caf235ebbb3f1798f
andrewjspivey <12spivey@gmail.com>
2021-09-15 15:32:52 -0700

    refactor done

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import axios from "axios";
 import "./App.css";
